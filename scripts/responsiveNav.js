ea8550094f66836bdc794dc8a22c8e756631d7d4
Juan Cardenas <juanfelipecardenas4@gmail.com>
2023-06-10 22:19:06 -0400

    Added:Colors, Added: Responsive navbar, Added: Creditcard input

---
+++ scripts/responsiveNav.js
@@ -0,0 +1,8 @@
+const HAMBURGER_MENU = document.querySelector(".hamburger__bar-container");
+
+HAMBURGER_MENU.addEventListener("click", mobileView);
+
+function mobileView(){
+    document.querySelector(".nav--mobile").classList.toggle('hidden');
+    HAMBURGER_MENU.classList.toggle('active');
+};
