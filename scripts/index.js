91f82e02a7842b31953a56bb91aa95dc798a6277
Juan Cardenas <juanfelipecardenas4@gmail.com>
2023-06-14 18:10:16 -0400

    Fixed: Luhn's Algorithm

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,41 @@
 const RESULT_TEXT = document.querySelector(".result-card__text");
+const RESULT_CARD = document.querySelector(".result-card__container");
 const CARD_INPUT = document.querySelector(".credit-card__input");
+const DEFAULT_CLASS = RESULT_CARD.classList.value;
 const MIN_CARD_LENGTH = 12;
 
 function logResult(isValid) {
   RESULT_TEXT.innerText = isValid ? "Valid card." : "Invalid card.";
+  RESULT_CARD.className = isValid
+    ? `${DEFAULT_CLASS}--valid`
+    : `${DEFAULT_CLASS}--invalid`;
 }
 
-CARD_INPUT.addEventListener("input", getCardNumber);
+CARD_INPUT.addEventListener("input", checkCardNumber);
 
-function getCardNumber() {
+function checkCardNumber() {
   if (!(this.value.length >= MIN_CARD_LENGTH)) {
+    RESULT_TEXT.innerText = "Please enter a card number above.";
+    RESULT_CARD.className = DEFAULT_CLASS;
     return;
   }
-  let cardNumber = this.value;
+  let cardNumber = reverseString(this.value);
   logResult(checkSum(cardNumber));
 }
 
 function checkSum(card) {
   let digits = splitIntoDigits(card);
 
-  for (let i = 0; i < digits.length; i += 2) {
+  for (let i = 1; i < digits.length; i += 2) {
     digits[i] *= 2;
     if (isTwoDigits(digits[i])) {
       digits[i] = sumDigits(digits[i]);
     }
   }
-  if(sumDigits(digits)%10 === 0){
+  if (sumDigits(digits) % 10 === 0) {
     return true;
   }
+  console.log(digits);
   return false;
 }
 
@@ -52,3 +60,7 @@ function sumDigits(number) {
   }
   return sum;
 }
+
+function reverseString(str) {
+    return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+  }
